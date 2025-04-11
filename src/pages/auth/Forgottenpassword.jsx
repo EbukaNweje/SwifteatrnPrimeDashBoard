1680a4390dfc7c31c4e142d3e3390951cd6b252b
EbukaNweje <nwejeebuka@gmail.com>
2025-04-11 14:30:38 -0700

    F

--- src/pages/auth/Forgottenpassword.jsx
+++ src/pages/auth/Forgottenpassword.jsx
@@ -46,15 +46,15 @@ const Forgottenpassword = () => {
         })
       }
 
-    //   const handleFacebook = (e) => {
-    //     window.location.href = "https://www.facebook.com/profile.php?id=61574843025485"
-    //   }
+      const handleFacebook = (e) => {
+        window.location.href = "https://www.facebook.com/profile.php?id=61574843025485"
+      }
 
     return (
         <div className="w-full h-screen">
             <div className="w-full h-12 bg-[#f8f8f8] phone:hidden px-48 flex items-center justify-between">
                 <div className="w-max flex items-center gap-4">
-                     <FacebookFilled onClick={"handleFacebook"}/>
+                     <FacebookFilled onClick={handleFacebook}/>
                 </div>
                 <div className="w-max flex items-center gap-5 text-sm">
                     <div className="w-max h-max cursor-pointer">Help</div>
